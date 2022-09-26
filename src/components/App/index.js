db41a8837ce009a789e57704e428de1af2623d03
Dimitri Ligdamis <ligdamisdimitri@gmail.com>
2022-09-26 15:43:39 +0200

    :bug: fix: Resolved bug after merge between menu and input

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,3 +1,5 @@
+import { Route, Routes } from 'react-router-dom';
+
 import LoginPage from '../Views/LoginPage';
 import Homepage from '../Views/Homepage';
 
@@ -6,9 +8,12 @@ import './styles.css';
 // == Composant
 function App() {
   return (
-      <div className="app">
-        <Homepage />
-      </div> 
+    <div className="app">
+      <Homepage />
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </div>
   );
 }
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,13 +3,13 @@ import { combineReducers } from 'redux';
 import userReducer from './user';
 import inputReducer from './input';
 import menuReducer from './menu';
-import sessionReducer from './session';
+// import sessionReducer from './session';
 
 const rootReducer = combineReducers({
   user: userReducer,
   input: inputReducer,
   menu: menuReducer,
-  session: sessionReducer, 
+  // session: sessionReducer,
 });
 
 export default rootReducer;
