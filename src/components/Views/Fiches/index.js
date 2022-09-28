d7bb4a971f570b090df948ac54a683405935f2d8
Dimitri Ligdamis <ligdamisdimitri@gmail.com>
2022-09-28 16:51:21 +0200

    :rocket: feat: New componnents Fiches + Cards + styles with static data

--- src/components/Views/Fiches/index.js
+++ src/components/Views/Fiches/index.js
@@ -1,14 +1,23 @@
+import Card from '../../Card';
 import Dashboard from '../../Dashboard';
 import Menu from '../../Menu';
 import './styles.scss';
 
 function Fiches() {
   return (
-    <main className="Fiches">
+    <>
       <Menu />
       <Dashboard />
-      <h1>Ici ce sont les Fiches</h1>
-    </main>
+      <main className="Fiches">
+        <h1>Recherche une fiche</h1>
+        <p>SearchBar ici</p>
+        <ul className="Fiches__list">
+          <Card />
+          <Card />
+          <Card />
+        </ul>
+      </main>
+    </>
   );
 }
 
