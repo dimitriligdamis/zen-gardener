5b69f9587a942deda39a7eb3d89ecd344f30e1f4
Dimitri Ligdamis <ligdamisdimitri@gmail.com>
2022-09-28 09:47:48 +0200

    :gear: config: Init branch dashboard

--- src/services/mockApi/session/index.js
+++ src/services/mockApi/session/index.js
@@ -11,46 +11,40 @@ const mockAdapter = (axios, apiUrl) => {
   mockAdapter
     // Mock failed login at first request
     .onPost(apiUrl)
-    .replyOnce(_ => {
-      return [
-        401, // HTTP 401 Unathorized
-        {
-          message: 'Unauthorized',
-        }
-      ];
-    })
+    .replyOnce((_) => [
+      401, // HTTP 401 Unathorized
+      {
+        message: 'Unauthorized',
+      },
+    ])
     // Mock successful login
     .onPost(apiUrl)
-    .reply(config => {
-      return [
-        200, // HTTP 200 OK
-        {
-          jwtToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c',
-          userData: {
-            id: 1234,
-            userName: "Igor",
-            email: "Igor@hossegor.fr",
-            address: "Rue de la vague",
-            city: "Hossegor",
-            postalCode: "40150",
-            phoneNumber: "0612345678",
-            sendTaskEmails: true,
-            sendWeeklyEmails: true,
-            updatedAt: Date.now(),
-          }
-        }
-      ];
-    })
+    .reply((config) => [
+      200, // HTTP 200 OK
+      {
+        jwtToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c',
+        userData: {
+          id: 1234,
+          userName: 'Igor',
+          email: 'Igor@hossegor.fr',
+          address: 'Rue de la vague',
+          city: 'Hossegor',
+          postalCode: '40150',
+          phoneNumber: '0612345678',
+          sendTaskEmails: true,
+          sendWeeklyEmails: true,
+          updatedAt: Date.now(),
+        },
+      },
+    ])
     // Mock log out request
     .onDelete(apiUrl)
-    .reply(_ => {
-      return [
-        204, // HTTP 204 No Content
-        {
-          message: 'Unauthorized',
-        }
-      ];
-    })
+    .reply((_) => [
+      204, // HTTP 204 No Content
+      {
+        message: 'Unauthorized',
+      },
+    ]);
 
   return axios;
 };
