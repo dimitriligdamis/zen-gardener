42f566496b684a45b0c011f25d7bd3988ee7385b
Dimitri Ligdamis <ligdamisdimitri@gmail.com>
2022-09-30 16:44:16 +0200

    :rocket: feat: Add profil Page

--- src/components/Views/Profile/index.js
+++ src/components/Views/Profile/index.js
@@ -1,12 +1,125 @@
-import Title from '../../Title';
+import { useState } from 'react';
+import { Edit } from 'react-feather';
+import { Link } from 'react-router-dom';
 import './styles.scss';
 
+const info = {
+  pseudo: 'Jéan Ver', email: 'Jean.ver@bonduel.fr', address: "8 rue de l'aubergine", zip_code: '87520', city: 'Le-potager', phone: '0800201010', task_notification: false, week_notification: false,
+};
+
+const {
+  pseudo, address, city, zip_code, phone, email, task_notification, week_notification,
+} = info;
+
 function Profile() {
+  const [inputDisabled, setInputDisabled] = useState(true);
+
+  // Value controlled input
+  const [inputPseudo, setInputPseudo] = useState(pseudo);
+  const [inputAddress, setInputAddress] = useState(address);
+  const [inputCity, setInputCity] = useState(city);
+  const [inputZipCode, setInputZipCode] = useState(zip_code);
+  const [inputPhone, setInputPhone] = useState(phone);
+  const [inputEmail, setInputEmail] = useState(email);
+
   return (
     <main className="Profile">
-      <Title>
-        <h1 className="Profile__title">Ici c'est la page Profil</h1>
-      </Title>
+      <Link className="Profile__logo" to="/tableaux">Logo</Link>
+      <h1 className="Profile__title">Page de profil de: {pseudo}</h1>
+      <section className="Profile__infos-container">
+        <h3 className="Profile__my-infos">Mes infos</h3>
+        <Edit
+          onClick={() => {
+            setInputDisabled(!inputDisabled);
+          }}
+          className="Profile__edit"
+        />
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            console.log({
+              ...info,
+              pseudo: inputPseudo,
+              email: inputEmail,
+              address: inputEmail,
+              zip_code: inputZipCode,
+              phone: inputPhone,
+              city: inputCity,
+            });
+          }}
+          className="Profile__form"
+        >
+          <ul className="Profile__infos">
+            <li className="Profile__info">
+              <label className="Profile__label">Pseudo:</label>
+              <input
+                onChange={(e) => {
+                  setInputPseudo(e.target.value);
+                }}
+                type="text"
+                value={inputPseudo}
+                disabled={inputDisabled}
+              />
+            </li>
+            <li className="Profile__info">
+              <label className="Profile__label">Adresse:</label>
+              <input
+                onChange={(e) => {
+                  setInputAddress(e.target.value);
+                }}
+                type="text"
+                value={inputAddress}
+                disabled={inputDisabled}
+              />
+            </li>
+            <li className="Profile__info">
+              <label className="Profile__label">Ville:</label>
+              <input
+                onChange={(e) => {
+                  setInputCity(e.target.value);
+                }}
+                type="text"
+                value={inputCity}
+                disabled={inputDisabled}
+              />
+            </li>
+            <li className="Profile__info">
+              <label className="Profile__label">Code postal:</label>
+              <input
+                onChange={(e) => {
+                  setInputZipCode(e.target.value);
+                }}
+                type="text"
+                value={inputZipCode}
+                disabled={inputDisabled}
+              />
+            </li>
+            <li className="Profile__info">
+              <label className="Profile__label">Téléphone:</label>
+              <input
+                onChange={(e) => {
+                  setInputPhone(e.target.value);
+                }}
+                type="text"
+                value={inputPhone}
+                disabled={inputDisabled}
+              />
+            </li>
+            <li className="Profile__info">
+              <label className="Profile__label">Mail:</label>
+              <input
+                onChange={(e) => {
+                  setInputEmail(e.target.value);
+                }}
+                type="text"
+                value={inputEmail}
+                disabled={inputDisabled}
+              />
+            </li>
+          </ul>
+          <button className="Profile__submit" type="submit" disabled={inputDisabled}>Valider les modifications</button>
+        </form>
+      </section>
     </main>
   );
 }
