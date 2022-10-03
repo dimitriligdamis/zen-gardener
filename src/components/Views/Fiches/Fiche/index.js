e8b230f15f55204c70d421023c0d6d6678382d7b
NicolasMRQS <nicolas08.10marques@gmail.com>
2022-10-03 15:41:19 +0200

    :rocket: feat: Fiche unique v0.2

--- src/components/Views/Fiches/Fiche/index.js
+++ src/components/Views/Fiches/Fiche/index.js
@@ -5,21 +5,27 @@ function Fiche() {
     code_fiche: 1,
     titre: 'Carotte Orange Dolciva (Nantaise Améliorée)',
     photos: 'https://kokopelli-semences.fr/media/cache/shop_product_large_thumbnail/p0602-1.jpg',
-    description: 'Cette variété – de type Nantaise améliorée – est une sélection de Sativa Rheinau, en Suisse, pour l’association Kultursaat. Ses longues racines cylindriques offrent une excellente saveur ainsi qu\'une texture très juteuse. Peu sensible à l\'éclatement, elle est adaptée pour les récoltes de saison et pour la conservation. Très grande vigueur au semis. Semer en place, en lignes espacées de 25 cm. Recouvrir peu et plomber légèrement. Éclaircir une première fois à 5 cm puis à 8 cm lorsque les plants ont 3 feuilles. Protéger, si possible, les jeunes pousses de l’appétit des limaces. Arroser régulièrement, mais légèrement, afin de favoriser la levée, puis plus abondamment 1 fois par semaine le mois qui suit le semis. Par temps sec, cela évitera que les racines ne se “creusent”.La récolte des carottes est possible toute l\'année - jusqu\'à floraison au printemps - si le sol ne gèle pas ou qu\'il est protégé par un paillage.',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque mauris orci, faucibus eu ante eget, cursus fermentum libero. Nulla venenatis cursus ullamcorper. Sed non diam non quam mollis gravida vel vitae nisl. Duis dignissim velit vel leo commodo iaculis. Curabitur sed diam euismod, volutpat arcu vel, luctus tortor. Vestibulum interdum ligula non ligula iaculis blandit. Donec dignissim porttitor tincidunt. Sed varius congue nibh quis efficitur. Nullam volutpat, tellus vitae pharetra tempus, metus sapien condimentum metus, quis porta magna massa sit amet massa. Proin feugiat sed nunc in pellentesque. Maecenas porttitor libero a egestas bibendum. Phasellus non eros consectetur, pulvinar nunc id, tristique justo. In hendrerit quam turpis, at ornare nunc lacinia semper. Mauris porta ante sed sapien finibus, sed rhoncus est interdum. Morbi lacus orci, fringilla in mauris eu, bibendum pharetra mauris. Vivamus semper suscipit dui, et ultrices lectus consectetur in. Maecenas laoreet, lacus ut bibendum venenatis, lectus nisi vehicula augue, in tincidunt lacus nibh ac metus. Nullam dignissim arcu nec arcu luctus varius. Morbi tincidunt lorem mauris, a tempor ipsum finibus at. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed in sollicitudin magna. Etiam placerat imperdiet erat, et lobortis mauris gravida tincidunt. Morbi maximus viverra nisi, sit amet efficitur libero hendrerit vestibulum. In ligula est, feugiat iaculis tortor et, vestibulum euismod sapien. Proin a fermentum ante. Vivamus sagittis mattis lectus, nec volutpat ipsum sodales a. Donec malesuada ut tellus ut egestas. In condimentum, mauris a accumsan vehicula, sapien leo condimentum mi, vitae euismod nunc mi eu lacus. Aliquam a dui malesuada sapien semper fringilla nec feugiat ipsum. Sed lectus arcu, iaculis ut erat sed, congue pretium arcu. Vivamus volutpat ligula sed nunc varius malesuada.',
     caracteristiques: '[Semis: pleine terre][Période de semis (pleine terre) : Février, Mars, Avril, Mai, Juin, Juillet][Période de récolte : Juin, Juillet, Août, Septembre, Octobre, Novembre][Culture : en pleine terre, en serre][Exposition : ensoleillée][Besoin en eau : moyen][Nature du sol : tout type de sol][Qualité du sol : drainé, meuble, riche]',
   };
 
-  const { titre, photos, description, caracteristiques } = infos;
+  const {
+    titre,
+    photos,
+    description,
+    caracteristiques,
+  } = infos;
 
   const turnCaracteristiquesInArray = (caract) => {
-    const caractSplit = caract.split(']');
+    let caractSplit = caract.split(']');
     caractSplit.pop();
-    return caractSplit.map((c) => c.substring(1));
+    caractSplit = caractSplit.map((c) => c.substring(1));
+    return caractSplit;
   };
 
   const caracteristiqueArray = turnCaracteristiquesInArray(caracteristiques);
 
-  console.log(caracteristiqueArray)
+  console.log(caracteristiqueArray);
 
   return (
     <section className="Fiche">
@@ -28,8 +34,8 @@ function Fiche() {
       </aside>
       <section className="Fiche__container">
         <h1 className="Fiche__title">{titre}</h1>
-        <img src={photos} />
-        <div>{description}</div>
+        <img className="Fiche__img" alt={titre} src={photos} />
+        <div className="Fiche__description">{description}</div>
       </section>
     </section>
   );
