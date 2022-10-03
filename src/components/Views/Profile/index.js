0cf93ec9aaeede7b7e381d8f1ec8bd64012af086
Dimitri Ligdamis <ligdamisdimitri@gmail.com>
2022-10-03 10:36:52 +0200

    :rocket: feat: Add Profil page with react hook form

--- src/components/Views/Profile/index.js
+++ src/components/Views/Profile/index.js
@@ -1,12 +1,13 @@
-import { useState } from 'react';
 import {
-  Edit, Framer, Mail, Map, MapPin, Phone, Tag,
+  Edit,
+  // Framer, Mail, Map, MapPin, Phone, Tag,
 } from 'react-feather';
+import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 import './styles.scss';
 
 const info = {
-  pseudo: 'Jéan Ver', email: 'Jean.ver@bonduel.fr', address: "8 rue de l'aubergine", zip_code: '87520', city: 'Le-potager', phone: '0800201010', task_notification: false, week_notification: false,
+  pseudo: 'Jéan Ver', email: 'Jean.ver@bonduel.fr', address: "8 rue de l'aubergine", zip_code: '87520', city: 'Le-potager', phone: '0800201010', task_notification: true, week_notification: true,
 };
 
 const {
@@ -14,15 +15,11 @@ const {
 } = info;
 
 function Profile() {
-  const [inputDisabled, setInputDisabled] = useState(true);
+  const {
+    register, handleSubmit, formState: { errors }, reset,
+  } = useForm();
 
-  // Value controlled input
-  const [inputPseudo, setInputPseudo] = useState(pseudo);
-  const [inputAddress, setInputAddress] = useState(address);
-  const [inputCity, setInputCity] = useState(city);
-  const [inputZipCode, setInputZipCode] = useState(zip_code);
-  const [inputPhone, setInputPhone] = useState(phone);
-  const [inputEmail, setInputEmail] = useState(email);
+  const onSubmit = handleSubmit((data) => console.log(data));
 
   return (
     <main className="Profile">
@@ -31,96 +28,44 @@ function Profile() {
       <section className="Profile__infos-container">
         <h3 className="Profile__my-infos">Mes infos</h3>
         <Edit
-          onClick={() => {
-            setInputDisabled(!inputDisabled);
-          }}
           className="Profile__edit"
         />
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            console.log({
-              ...info,
-              pseudo: inputPseudo,
-              email: inputEmail,
-              address: inputEmail,
-              zip_code: inputZipCode,
-              phone: inputPhone,
-              city: inputCity,
-            });
-          }}
-          className="Profile__form"
-        >
-          <ul className="Profile__infos">
-            <li className="Profile__info">
-              <label className="Profile__label"><Tag /><em>Pseudo:</em></label>
-              <input
-                onChange={(e) => {
-                  setInputPseudo(e.target.value);
-                }}
-                type="text"
-                value={inputPseudo}
-                disabled={inputDisabled}
-              />
-            </li>
-            <li className="Profile__info">
-              <label className="Profile__label"><MapPin /><em>Adresse:</em></label>
-              <input
-                onChange={(e) => {
-                  setInputAddress(e.target.value);
-                }}
-                type="text"
-                value={inputAddress}
-                disabled={inputDisabled}
-              />
-            </li>
-            <li className="Profile__info">
-              <label className="Profile__label"><Map /><em>Ville:</em></label>
-              <input
-                onChange={(e) => {
-                  setInputCity(e.target.value);
-                }}
-                type="text"
-                value={inputCity}
-                disabled={inputDisabled}
-              />
-            </li>
-            <li className="Profile__info">
-              <label className="Profile__label"><Framer /><em>Code postal:</em></label>
-              <input
-                onChange={(e) => {
-                  setInputZipCode(e.target.value);
-                }}
-                type="text"
-                value={inputZipCode}
-                disabled={inputDisabled}
-              />
-            </li>
-            <li className="Profile__info">
-              <label className="Profile__label"><Phone /><em>Téléphone:</em></label>
-              <input
-                onChange={(e) => {
-                  setInputPhone(e.target.value);
-                }}
-                type="text"
-                value={inputPhone}
-                disabled={inputDisabled}
-              />
-            </li>
-            <li className="Profile__info">
-              <label className="Profile__label"><Mail /><em>Mail:</em></label>
-              <input
-                onChange={(e) => {
-                  setInputEmail(e.target.value);
-                }}
-                type="text"
-                value={inputEmail}
-                disabled={inputDisabled}
-              />
-            </li>
-          </ul>
-          <button className="Profile__submit" type="submit" disabled={inputDisabled}>Valider les modifications</button>
+        <form onSubmit={onSubmit}>
+          <article className="Profile__item">
+            <label className="Profile__label" htmlFor="pseudo">Pseudo</label>
+            <input value={pseudo} className="Profile__input" id="pseudo" {...register('pseudo')} />
+          </article>
+          <article className="Profile__item">
+            <label className="Profile__label" htmlFor="address">Adresse</label>
+            <input value={address} className="Profile__input" id="address" {...register('address')} />
+          </article>
+          <article className="Profile__item">
+            <label className="Profile__label" htmlFor="city">Ville</label>
+            <input value={city} className="Profile__input" id="city" {...register('city')} />
+          </article>
+          <article className="Profile__item">
+            <label className="Profile__label" htmlFor="zip_code">Code postal</label>
+            <input value={zip_code} className="Profile__input" id="zip_code" {...register('zip_code')} />
+          </article>
+          <article className="Profile__item">
+            <label className="Profile__label" htmlFor="phone">Téléphone</label>
+            <input value={phone} className="Profile__input" id="phone" {...register('phone')} />
+          </article>
+          <article className="Profile__item">
+            <label className="Profile__label" htmlFor="email">Mail</label>
+            <input value={email} className="Profile__input" id="email" {...register('email')} />
+          </article>
+          <article className="Profile__item">
+            <label className="Profile__label" htmlFor="task_notification">Envoyez moi un email par tâches</label>
+            <input checked={task_notification} type="checkbox" className="Profile__input" id="task_notification" {...register('task_notification')} />
+          </article>
+          <article className="Profile__item">
+            <label className="Profile__label" htmlFor="week_notification">Envoyez moi un email par semaine</label>
+            <input checked={week_notification} type="checkbox" className="Profile__input" id="week_notification" {...register('week_notification')} />
+          </article>
+          <button className="Profile__submit" type="submit">Valider</button>
         </form>
+
       </section>
     </main>
   );
